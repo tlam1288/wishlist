23955abe800b446cf3d44c11b906d788ed190587
nhuanthi <tlam1288@gmail.com>
2021-02-07 12:09:58 -0800

    Updates

---
+++ precache-manifest.cad2db7dd3d7db9b53d55aaded389ff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8874d8dc679f2e14646f57b6b3fbacf0",
+    "url": "/wishlist/index.html"
+  },
+  {
+    "revision": "65b7e94d2946ab7eb241",
+    "url": "/wishlist/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "ee14b54770aadcbe3a2a",
+    "url": "/wishlist/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "65b7e94d2946ab7eb241",
+    "url": "/wishlist/static/js/2.7d43b46f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wishlist/static/js/2.7d43b46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee14b54770aadcbe3a2a",
+    "url": "/wishlist/static/js/main.f444bcff.chunk.js"
+  },
+  {
+    "revision": "430cd39c2f7f10a45102",
+    "url": "/wishlist/static/js/runtime-main.508c4cee.js"
+  }
+]);
